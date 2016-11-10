f4d888401f905dda3e23c7890a8060569599d08a
Filipe P. Fraga <lip3pacheco@gmail.com>
2016-11-10 21:01:34 -0200

    Correção pois o projeto foi inicializado de forma errada.

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h> //Include para possibilitar o uso de acentuação
 
 int main()
 {
-    printf("Hello world!\n");
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("AUTORES: Filipe Pacheco de Fraga e Marcelo Audibert Chaves\n");
+
+
     return 0;
 }
