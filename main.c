47984c0c777e317414e2fa69059c18c6562dd309
Filipe P. Fraga <lip3pacheco@gmail.com>
2016-11-10 22:15:17 -0200

    Criado um header com a implementação do menu do jogo, já tratando a entrada do usuário.

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
 #include <locale.h> //Include para possibilitar o uso de acentuação
+#include "header.h"
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    printf("AUTORES: Filipe Pacheco de Fraga e Marcelo Audibert Chaves\n");
-
+    //Switch em cima do retorno da função do menu, que retorna a entrada do usuário direcionando-o para o respectivo menu
+    switch(menuPrincipal())
+    {
+        case 1: //Clássico
+            break;
+        case 2: //Fácil
+            break;
+        case 3: //Personalizado
+            break;
+        case 4: //Rank
+            break;
+    }
 
     return 0;
 }
